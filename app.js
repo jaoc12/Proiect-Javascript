629cb91cc11e60c0f7b59e8666f2ce4d371ca2f3
jaoc12 <chitu.stefan@yahoo.com>
2019-11-14 16:49:13 +0200

    Prima versiune

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('html/index');
+});
+app.get('/program', function(req, res) {
+    res.render('html/program');
+});
+app.get('/filme', function(req, res) {
+    res.render('html/filme');
+});
+app.get('/promotii', function(req, res) {
+    res.render('html/promotii');
+});
+
+app.use('/css', express.static('css'));
+app.use('/imagini', express.static('imagini'));
+
+app.listen(8080);
+console.log('Aplicatia se va deschide pe portul 8080.');
