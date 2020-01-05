640376b6c49cf986c82662747d95761b40d88728
jaoc12 <chitu.stefan@yahoo.com>
2020-01-05 13:33:25 +0200

    Adaugat checkbox

--- script/script-filme.js
+++ script/script-filme.js
@@ -5,6 +5,18 @@ window.onload = function() {
 	document.getElementById("viitor").onclick = function() {
 		showHide("container-acum","container-viitor");
 	};
+	document.getElementById("Drama").onchange = function(){
+		showGen("Drama");
+	};
+	document.getElementById("Thriller").onchange = function(){
+		showGen("Thriller");
+	};
+	document.getElementById("Actiune").onchange = function(){
+		showGen("Actiune");
+	};
+	document.getElementById("Comedie").onchange = function(){
+		showGen("Comedie");
+	};
 };
 
 function showHide(container1, container2) {
@@ -12,4 +24,24 @@ function showHide(container1, container2) {
 	var y = document.getElementById(container2);
 	x.style.display = "none";
 	y.style.display = "block";
+};
+
+function showGen(gen){
+	var adevarat = document.getElementById(gen).checked;
+	var divuri = document.getElementsByClassName("container");
+	for(var dv of divuri){
+		var filme = dv.getElementsByClassName("film");
+		for(var film of filme){
+			var tduri = film.getElementsByTagName("td");
+			var genuri = tduri[0].innerHTML;
+			if(genuri.includes(gen)){
+				if(adevarat){
+					film.style.display = "block";
+				}
+				else{
+					film.style.display = "none";
+				}
+			}
+		}
+	}
 };
